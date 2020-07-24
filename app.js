5213ac1e7cfe24b8d4146388849c4a37c380e64c
ashleylaswell <lswl9898@gmail.com>
2020-07-24 18:33:19 -0400

    changed grid size

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let container = document.getElementById('container');
 let limit = true;
 while (limit){
-  let gridSize = prompt("How many blocks wide would you like your grid to be? (200 or less)")
-  if (gridSize > 1 && gridSize <= 200){
+  let gridSize = prompt("How many blocks wide would you like your grid to be? (100 or less)")
+  if (gridSize > 1 && gridSize <= 100){
     createGrid(gridSize)
     limit = false;
   }
@@ -17,7 +17,6 @@ function createGrid(size){
   }
   
 }
-
 
 let boxes = document.getElementsByClassName("grid-item");
 function drawGrid(){
