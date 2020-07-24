dae9cf672bcc52a6a8bfedbb609d32e2f5d804ca
ashleylaswell <lswl9898@gmail.com>
2020-07-24 14:07:59 -0400

    added hover that changes background color of div and reload page button

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-let container = document.querySelector(".container");
-// let gridSize = prompt("How many blocks wide would you like your grid to be?")
+let container = document.getElementById('container');
+let gridSize = prompt("How many blocks wide would you like your grid to be?")
 function createGrid(size){
   container.style.setProperty('--size', size);
-  for(var i = 0; i < size * size; i++){
+  for(let i = 0; i < size * size; i++){
     let box = document.createElement('div');
     box.className="grid-item";
     container.appendChild(box);
-    
   }
   
 }
 
-createGrid(16);
+createGrid(gridSize);
+
+let boxes = document.getElementsByClassName("grid-item");
+for (let i = 0; i < boxes.length; i++){
+  boxes[i].addEventListener("mouseover", function(e){
+    this.style.backgroundColor = "black";
+  })
+}
